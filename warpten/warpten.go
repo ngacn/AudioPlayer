3df2bc04a232da32a1a2858a8c4f20d81f94a587
pocari-- <eggsspamham@gmail.com>
2014-11-19 23:59:36 +0800

    暂时移除了client
    模拟docker的做法，守护执行warpten开启服务器，
    非守护模式执行warpten相当于cli端。

--- warpten/warpten.go
+++ warpten/warpten.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net"
+	"strings"
 	"warpten/server"
 )
 
 func main() {
-	srv, err := server.NewWarptenSrv("tcp", "127.0.0.1:7777")
-	if err != nil {
-		fmt.Println("Couldn't create WarptenSrv")
+	const (
+		DEFAULTHTTPHOST   = "127.0.0.1:7478"
+		DEFAULTUNIXSOCKET = "/var/run/warpten.sock"
+	)
+
+	var (
+		flDaemon = flag.Bool("d", false, "Enable daemon mode")
+		flTcp    = flag.Bool("t", false, "Enable the TCP socket")
+		flCmd    = flag.String("s", "PLAY", "Send a command to server")
+	)
+
+	flag.Parse()
+
+	defaultHost := fmt.Sprintf("unix://%s", DEFAULTUNIXSOCKET)
+	if *flTcp {
+		defaultHost = fmt.Sprintf("tcp://%s", DEFAULTHTTPHOST)
+	}
+
+	protoAddrParts := strings.SplitN(defaultHost, "://", 2)
+
+	if *flDaemon {
+		srv, err := server.NewWarptenSrv(protoAddrParts[0], protoAddrParts[1])
+		if err != nil {
+			fmt.Println("WarptenSrv: Couldn't create WarptenSrv")
+			return
+		}
+		srv.Serve()
 		return
 	}
-	srv.Serve()
+
+	conn, err := net.Dial(protoAddrParts[0], protoAddrParts[1])
+	if err != nil {
+		fmt.Println("WarptenCli: Couldn't connect to server")
+		return
+	}
+	fmt.Fprintln(conn, *flCmd)
 }
