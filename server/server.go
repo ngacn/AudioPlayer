1a220a5cea5de080cc1a0c3599e73a3e7749cf0c
pocari-- <eggsspamham@gmail.com>
2014-11-20 00:17:42 +0800

    实现服务器端接受http协议指令
    其实就是在抄docker。。客户端还没改

--- server/server.go
+++ server/server.go
@@ -4,46 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"net/http"
+	"os"
+	"syscall"
 )
 
+type HttpApiFunc func(w http.ResponseWriter, r *http.Request) error
+
+func createRouter() (*http.ServeMux, error) {
+	r := http.NewServeMux()
+	m := map[string]map[string]HttpApiFunc{
+		"GET": {
+			"/version": getVersion,
+		},
+		"POST":   {},
+		"DELETE": {},
+	}
+	for method, routes := range m {
+		for route, fct := range routes {
+			localRoute := route
+			localFct := fct
+			localMethod := method
+			f := makeHttpHandler(localMethod, localRoute, localFct)
+			r.HandleFunc(localRoute, f)
+		}
+	}
+
+	return r, nil
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) error {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "yoyoyoyo")
+	return nil
+}
+
+func makeHttpHandler(localMethod string, localRoute string, handlerFunc HttpApiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := handlerFunc(w, r); err != nil {
+			fmt.Println("WarptenSrv: handlerFunc failed.")
+		}
+	}
+}
+
 type Server interface {
-	Serve()
+	Serve() error
 	Close() error
 }
 
 type HttpServer struct {
-	l net.Listener
+	srv *http.Server
+	l   net.Listener
 }
 
-func (s *HttpServer) Serve() {
-	for {
-		conn, err := s.l.Accept()
-		if err != nil {
-			continue
-		}
-		go s.handleConnection(conn)
-	}
+func (s *HttpServer) Serve() error {
+	return s.srv.Serve(s.l)
 }
 
 func (s *HttpServer) Close() error {
 	return s.l.Close()
-}
-
-func (s *HttpServer) handleConnection(conn net.Conn) {
-	for {
-		reader := bufio.NewReader(conn)
-		cmd, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
-        if cmd == "EXIT\n" {
-            fmt.Println("Exit ...")
-            break
-        } else {
-            fmt.Printf(cmd)
-        }
-	}
-	conn.Close()
 }
 
 func NewWarptenSrv(proto, addr string) (Server, error) {
@@ -58,13 +77,38 @@ func NewWarptenSrv(proto, addr string) (Server, error) {
 }
 
 func setupTcpHttp(addr string) (*HttpServer, error) {
+	r, err := createRouter()
+	if err != nil {
+		return nil, err
+	}
+
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
-	return &HttpServer{l}, nil
+
+	return &HttpServer{&http.Server{Addr: addr, Handler: r}, l}, nil
 }
 
 func setupUnixHttp(addr string) (*HttpServer, error) {
-	return nil, nil
+	r, err := createRouter()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := syscall.Unlink(addr); err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+	mask := syscall.Umask(0777)
+	defer syscall.Umask(mask)
+
+	l, err := net.Listen("unix", addr)
+	if err != nil {
+		return nil, err
+	}
+	if err := os.Chmod(addr, 0660); err != nil {
+		return nil, err
+	}
+
+	return &HttpServer{&http.Server{Addr: addr, Handler: r}, l}, nil
 }
